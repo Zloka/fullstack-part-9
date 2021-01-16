1225cfdeb45e146d02488e6c090576fa2fc0ee4f
Zacharias Levander <zacharias.levander@mafy.fi>
2021-01-16 11:50:21 +0200

    Add endpoint for bmi
    This marks my answer for exercise 9.5.

--- index.ts
+++ index.ts
@@ -1,8 +1,22 @@
 import express from 'express';
+import { calculateBmi } from './modules/bmiCalculator';
+
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query;
+  const invalidArguments = !height || !weight || isNaN(Number(height)) || isNaN(Number(weight));
+
+  if (invalidArguments) {
+    res.status(400).send('Arguments are invalid, expected height and weight to be numbers');
+  }
+  const heightAsNum = Number(height);
+  const weightAsNum = Number(weight);
+  res.json({ height: heightAsNum, weight: weightAsNum, bmi: calculateBmi(heightAsNum, weightAsNum)});
 });
 
 const PORT = 3003;
---
+++ modules/bmiCalculator.ts
@@ -0,0 +1,18 @@
+const calculateBmi = (height: number, weight: number) => {
+  const heightInMetres = height / 100;
+  const bmi = weight / (heightInMetres * heightInMetres);
+
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi > 18.5 && bmi < 25) {
+    return "Normal (healthy weight)";
+  } else if (bmi > 25 && bmi < 30) {
+    return "Overweight";
+  } else {
+    return "Obesity";
+  }
+}
+
+export {
+  calculateBmi
+}

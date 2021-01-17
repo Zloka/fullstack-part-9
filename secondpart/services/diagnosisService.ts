0c1150a5f6bdfda67b9ae25f08c71da1b42f40ed
Zacharias Levander <zacharias.levander@mafy.fi>
2021-01-17 11:05:01 +0200

    Expand the types with the new types
    This marks my answer for exercise 9.19.

--- secondpart/services/diagnosisService.ts
+++ secondpart/services/diagnosisService.ts
@@ -1,7 +1,7 @@
 import diagnoses from '../data/diagnoses.json';
-import { Diagnose } from '../types';
+import { Diagnosis } from '../types';
 
-const getDiagnoses = (): Diagnose[] => {
+const getDiagnoses = (): Diagnosis[] => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return diagnoses;
 };
